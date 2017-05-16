98ff4d711bb7687710c61650973d0784017bc039
sudheer <sudheer.puthineedi@gmail.com>
2017-05-16 15:46:46 +0530

    PowerShell syntax for database backup from local

---
+++ DatabaseBackupUsingPowerShell.ps1
@@ -0,0 +1,10 @@
+#Use the following command to take backup of your local database
+
+Syntax: 
+    Backup-Database -ServerInstance InstanceName -Database DatabaseName [-BackupAction] [Database] -> optional
+
+Example:
+    (For Recruitment)
+    Backup-SqlDatabase -ServerInstance QATVDCSD009\SQLEXPRESS -Database QRecruitment -BackupAction Database
+    (For MyChoices)
+    Backup-SqlDatabase -ServerInstance QATVDCSD009\SQL2014 -Database WhitePage.MainDB -BackupAction Database
