858aa83b36a0d8463c3923d15d099ca644855900
sudheer <sudheer.puthineedi@gmail.com>
2017-05-16 15:47:58 +0530

    Update DatabaseBackupUsingPowerShell.ps1

--- DatabaseBackupUsingPowerShell.ps1
+++ DatabaseBackupUsingPowerShell.ps1
@@ -1,10 +1,12 @@
 #Use the following command to take backup of your local database
 
+<*
 Syntax: 
     Backup-Database -ServerInstance InstanceName -Database DatabaseName [-BackupAction] [Database] -> optional
+*>
 
-Example:
-    (For Recruitment)
+<*Example:
+    (For Recruitment)*>
     Backup-SqlDatabase -ServerInstance QATVDCSD009\SQLEXPRESS -Database QRecruitment -BackupAction Database
-    (For MyChoices)
+    <*(For MyChoices)*>
     Backup-SqlDatabase -ServerInstance QATVDCSD009\SQL2014 -Database WhitePage.MainDB -BackupAction Database
