427fe2f79e7e4006647e0ce77f3a291097400b53
sudheer <sudheer.puthineedi@gmail.com>
2017-05-23 19:11:20 +0530

    Added validations and reading input from host

--- DeployWebApplicationToIIS.ps1
+++ DeployWebApplicationToIIS.ps1
@@ -5,10 +5,48 @@ Import-Module WebAdministration
    
    Creates QRecruitment http://localhost:5900
 #>
-[string] $recruitment = 'QRecruitment'
+$siteName = Read-Host 'Please enter website name'
 
-New-WebAppPool $recruitment
+if ($siteName -eq "") { throw  "Website name shouldn't be empty"  }
 
-New-Website -Name $recruitment -port 5900 -PhysicalPath 'C:\DevOps\QRSample'
+$port = Read-Host 'Please enter port number'
+
+if ($port -eq "") { throw  "Port value shouldn't be empty"  }
+
+$path = Read-Host 'Enter physical path'
+
+if ($path -eq "") { throw  "Path value shouldn't be empty"  }
+
+try{
+	#Removing application if it is already exists
+	
+	if (Test-Path "IIS\sites\$siteName"){
+		Remove-Website $siteName
+	}
+
+	#Removing application pool if already exists
+
+	if (Test-Path "IIS\sites\$siteName"){
+		Remove-WebAppPool $siteName
+	}
+
+	foreach($name in Get-ChildItem "IIS\sites"){
+		if ($name.Bindings.Collection.bindingInformation -eq ("*:" + $port + ":")){
+			"Found an existing site $site.Name with port $port, deleting it"
+			Remove-Website $site.Name
+		}
+	}
+	
+	#Creating a new pool
+		 
+	New-WebAppPool $siteName
+
+	#Creating a new application
+
+	New-Website -Name $siteName -port $port -PhysicalPath $path
+
+}catch{
+	throw "Error occured while deploying $siteName"
+}
 
 Write-Host 'Site created'
